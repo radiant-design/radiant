afa4440c3c05ff30bd297d423e2ce5a6e3e3eb7b
Kunal Pant <kunalpant@intugine.com>
2022-06-06 22:37:01 +0530

    reverting changes

--- src/Chip/Chip.tsx
+++ src/Chip/Chip.tsx
@@ -224,11 +224,7 @@ const Chip = React.forwardRef(function Chip(inProps, ref) {
 
   const ownerState = {
     ...props,
-    children,
-    className,
-    componentsProps: {},
-    startDecorator,
-    endDecorator,
+
     component,
     onClick,
     disabled,
