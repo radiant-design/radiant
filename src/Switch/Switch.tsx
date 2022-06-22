cf6752295483dea39c2330aef1d9c0f01a47aa85
Kunal Pant <kunalpant@intugine.com>
2022-06-22 18:02:59 +0530

    switch css

--- src/Switch/Switch.tsx
+++ src/Switch/Switch.tsx
@@ -1,30 +1,33 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import clsx from 'clsx';
-import { unstable_capitalize as capitalize } from '@mui/utils';
-import { unstable_composeClasses as composeClasses } from '@mui/base';
-import { useSwitch } from '@mui/base/SwitchUnstyled';
-import { styled, Theme } from '../styles';
-import switchClasses, { getSwitchUtilityClass } from './switchClasses';
-import { SwitchProps } from './SwitchProps';
+import * as React from "react";
+import PropTypes from "prop-types";
+import clsx from "clsx";
+import { unstable_capitalize as capitalize } from "@mui/utils";
+import { unstable_composeClasses as composeClasses } from "@mui/base";
+import { useSwitch } from "@mui/base/SwitchUnstyled";
+import { styled, Theme } from "../styles";
+import switchClasses, { getSwitchUtilityClass } from "./switchClasses";
+import { SwitchProps } from "./SwitchProps";
 
-const useUtilityClasses = (ownerState: SwitchProps & { focusVisible: boolean }) => {
-  const { checked, disabled, focusVisible, readOnly, color, variant } = ownerState as any;
+const useUtilityClasses = (
+  ownerState: SwitchProps & { focusVisible: boolean }
+) => {
+  const { checked, disabled, focusVisible, readOnly, color, variant } =
+    ownerState as any;
 
   const slots = {
     root: [
-      'root',
-      checked && 'checked',
-      disabled && 'disabled',
-      focusVisible && 'focusVisible',
-      readOnly && 'readOnly',
+      "root",
+      checked && "checked",
+      disabled && "disabled",
+      focusVisible && "focusVisible",
+      readOnly && "readOnly",
       variant && `variant${capitalize(variant)}`,
       color && `color${capitalize(color)}`,
     ],
-    thumb: ['thumb', checked && 'checked'],
-    track: ['track', checked && 'checked'],
-    action: ['action', focusVisible && 'focusVisible'],
-    input: ['input'],
+    thumb: ["thumb", checked && "checked"],
+    track: ["track", checked && "checked"],
+    action: ["action", focusVisible && "focusVisible"],
+    input: ["input"],
   };
 
   return composeClasses(slots, getSwitchUtilityClass, {});
@@ -32,94 +35,102 @@ const useUtilityClasses = (ownerState: SwitchProps & { focusVisible: boolean }) 
 
 const switchColorVariables =
   ({ theme, ownerState }: { theme: Theme; ownerState: SwitchProps }) =>
-  (data: { state?: 'Hover' | 'Disabled'; checked?: boolean } = {}) => {
+  (data: { state?: "Hover" | "Disabled"; checked?: boolean } = {}) => {
     const variant = ownerState.variant;
     const color = ownerState.color;
     return {
-      '--Switch-track-background': theme.vars.palette[color!]?.[`${variant!}${data.state || ''}Bg`],
-      '--Switch-track-color':
-        ownerState.variant === 'solid' ? '#fff' : theme.vars.palette[color!]?.plainColor,
-      '--Switch-track-borderColor':
-        variant === 'outlined'
-          ? theme.vars.palette[color!]?.[`${variant!}${data.state || ''}Border`]
-          : 'currentColor',
-      '--Switch-thumb-background':
-        theme.vars.palette[color!]?.[`${variant!}${data.state || ''}Color`],
-      '--Switch-thumb-color':
-        ownerState.variant === 'solid' ? theme.vars.palette[color!]?.plainColor : '#fff',
+      "--Switch-track-background":
+        theme.vars.palette[color!]?.[`${variant!}${data.state || ""}Bg`],
+      "--Switch-track-color":
+        ownerState.variant === "solid"
+          ? "#fff"
+          : theme.vars.palette[color!]?.plainColor,
+      "--Switch-track-borderColor":
+        variant === "outlined"
+          ? theme.vars.palette[color!]?.[`${variant!}${data.state || ""}Border`]
+          : "currentColor",
+      "--Switch-thumb-background":
+        theme.vars.palette[color!]?.[`${variant!}${data.state || ""}Color`],
+      "--Switch-thumb-color":
+        ownerState.variant === "solid"
+          ? theme.vars.palette[color!]?.plainColor
+          : "#fff",
     };
   };
 
-const SwitchRoot = styled('span', {
-  name: 'JoySwitch',
-  slot: 'Root',
+const SwitchRoot = styled("span", {
+  name: "JoySwitch",
+  slot: "Root",
   overridesResolver: (props, styles) => styles.root,
 })<{ ownerState: SwitchProps }>(({ theme, ownerState }) => {
   const getColorVariables = switchColorVariables({ theme, ownerState });
   return {
-    ...(ownerState.variant === 'outlined' && theme.variants.outlined[ownerState.color!]),
-    '--Switch-track-radius': theme.vars.radius.lg,
-    '--Switch-thumb-shadow':
-      ownerState.variant === 'soft' ? 'none' : '0 0 0 1px var(--Switch-track-background)', // create border-like if the thumb is bigger than the track
-    ...(ownerState.size === 'sm' && {
-      '--Switch-track-width': '40px',
-      '--Switch-track-height': '20px',
-      '--Switch-thumb-size': '12px',
-      '--Switch-decorator-paddingX': '4px',
+    ...(ownerState.variant === "outlined" &&
+      theme.variants.outlined[ownerState.color!]),
+    "--Switch-track-radius": theme.vars.radius.lg,
+    "--Switch-thumb-shadow":
+      ownerState.variant === "soft"
+        ? "none"
+        : "0 0 0 1px var(--Switch-track-background)", // create border-like if the thumb is bigger than the track
+    ...(ownerState.size === "sm" && {
+      "--Switch-track-width": "28px", //40
+      "--Switch-track-height": "16px", //20
+      "--Switch-thumb-size": "12px",
+      "--Switch-decorator-paddingX": "4px",
       fontSize: theme.vars.fontSize.sm,
     }),
-    ...(ownerState.size === 'md' && {
-      '--Switch-track-width': '48px',
-      '--Switch-track-height': '24px',
-      '--Switch-thumb-size': '16px',
-      '--Switch-decorator-paddingX': '8px',
+    ...(ownerState.size === "md" && {
+      "--Switch-track-width": "36px", //48
+      "--Switch-track-height": "20px", //24
+      "--Switch-thumb-size": "16px",
+      "--Switch-decorator-paddingX": "8px",
       fontSize: theme.vars.fontSize.md,
     }),
-    ...(ownerState.size === 'lg' && {
-      '--Switch-track-width': '64px',
-      '--Switch-track-height': '32px',
-      '--Switch-thumb-size': '24px',
-      '--Switch-decorator-paddingX': '12px',
+    ...(ownerState.size === "lg" && {
+      "--Switch-track-width": "44px", //64
+      "--Switch-track-height": "24px", //32
+      "--Switch-thumb-size": "20px", //24
+      "--Switch-decorator-paddingX": "12px",
     }),
-    '--Switch-thumb-radius': 'calc(var(--Switch-track-radius) - 2px)',
-    '--Switch-thumb-width': 'var(--Switch-thumb-size)',
-    '--Switch-thumb-offset':
-      'max((var(--Switch-track-height) - var(--Switch-thumb-size)) / 2, 0px)',
+    "--Switch-thumb-radius": "calc(var(--Switch-track-radius) - 2px)",
+    "--Switch-thumb-width": "var(--Switch-thumb-size)",
+    "--Switch-thumb-offset":
+      "max((var(--Switch-track-height) - var(--Switch-thumb-size)) / 2, 0px)",
     ...getColorVariables(),
-    '&:hover': {
-      ...getColorVariables({ state: 'Hover' }),
-    },
+    // "&:hover": {
+    //   ...getColorVariables({ state: "Hover" }),
+    // },
     [`&.${switchClasses.checked}`]: {
       ...getColorVariables({ checked: true }),
-      '&:hover': {
-        ...getColorVariables({ checked: true, state: 'Hover' }),
-      },
+      // "&:hover": {
+      //   ...getColorVariables({ checked: true, state: "Hover" }),
+      // },
     },
     [`&.${switchClasses.disabled}`]: {
-      pointerEvents: 'none',
+      pointerEvents: "none",
       color: theme.vars.palette.text.tertiary,
       //@ts-ignore
-      ...getColorVariables({ state: 'Disabled', checked: ownerState.checked }),
+      ...getColorVariables({ state: "Disabled", checked: ownerState.checked }),
     },
-    display: 'inline-flex',
-    alignItems: 'center',
-    alignSelf: 'center',
+    display: "inline-flex",
+    alignItems: "center",
+    alignSelf: "center",
     fontFamily: theme.vars.fontFamily.body,
-    position: 'relative',
+    position: "relative",
     padding:
-      'calc((var(--Switch-thumb-size) / 2) - (var(--Switch-track-height) / 2)) calc(-1 * var(--Switch-thumb-offset))',
-    backgroundColor: 'initial', // clear background in case `outlined` variant contain background.
-    border: 'none',
+      "calc((var(--Switch-thumb-size) / 2) - (var(--Switch-track-height) / 2)) calc(-1 * var(--Switch-thumb-offset))",
+    backgroundColor: "initial", // clear background in case `outlined` variant contain background.
+    border: "none",
   };
 });
 
-const SwitchAction = styled('div', {
-  name: 'JoySwitch',
-  slot: 'Action',
+const SwitchAction = styled("div", {
+  name: "JoySwitch",
+  slot: "Action",
   overridesResolver: (props, styles) => styles.action,
 })<{ ownerState: SwitchProps }>(({ theme }) => ({
-  borderRadius: 'var(--Switch-track-radius)',
-  position: 'absolute',
+  borderRadius: "var(--Switch-track-radius)",
+  position: "absolute",
   top: 0,
   left: 0,
   bottom: 0,
@@ -127,92 +138,100 @@ const SwitchAction = styled('div', {
   [theme.focus.selector]: theme.focus.default,
 }));
 
-const SwitchInput = styled('input', {
-  name: 'JoySwitch',
-  slot: 'Input',
+const SwitchInput = styled("input", {
+  name: "JoySwitch",
+  slot: "Input",
   overridesResolver: (props, styles) => styles.input,
 })<{ ownerState: SwitchProps }>({
   margin: 0,
-  height: '100%',
-  width: '100%',
+  height: "100%",
+  width: "100%",
   opacity: 0,
-  position: 'absolute',
-  cursor: 'pointer',
+  position: "absolute",
+  cursor: "pointer",
 });
 
-const SwitchTrack = styled('span', {
-  name: 'JoySwitch',
-  slot: 'Track',
+const SwitchTrack = styled("span", {
+  name: "JoySwitch",
+  slot: "Track",
   overridesResolver: (props, styles) => styles.track,
-})<{ ownerState: SwitchProps & { focusVisible: boolean } }>(({ theme, ownerState }) => ({
-  position: 'relative',
-  color: 'var(--Switch-track-color)',
-  height: 'var(--Switch-track-height)',
-  width: 'var(--Switch-track-width)',
-  display: 'flex',
-  justifyContent: 'space-between',
-  alignItems: 'center',
-  boxSizing: 'border-box',
-  border: 'var(--variant-outlinedBorderWidth, 0px) solid',
-  borderColor: 'var(--Switch-track-borderColor)',
-  backgroundColor: 'var(--Switch-track-background)',
-  borderRadius: 'var(--Switch-track-radius)',
-  fontFamily: theme.vars.fontFamily.body,
-  ...(ownerState.size === 'sm' && {
-    fontSize: theme.vars.fontSize.xs,
-  }),
-  ...(ownerState.size === 'md' && {
-    fontSize: theme.vars.fontSize.sm,
-  }),
-  ...(ownerState.size === 'lg' && {
-    fontSize: theme.vars.fontSize.md,
-  }),
+})<{ ownerState: SwitchProps & { focusVisible: boolean } }>(
+  ({ theme, ownerState }) => ({
+    position: "relative",
+    color: "var(--Switch-track-color)",
+    height: "var(--Switch-track-height)",
+    width: "var(--Switch-track-width)",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    boxSizing: "border-box",
+    border: "var(--variant-outlinedBorderWidth, 0px) solid",
+    borderColor: "var(--Switch-track-borderColor)",
+    backgroundColor: "var(--Switch-track-background)",
+    borderRadius: "var(--Switch-track-radius)",
+    fontFamily: theme.vars.fontFamily.body,
+    ...(ownerState.size === "sm" && {
+      fontSize: theme.vars.fontSize.sm, //sx
+    }),
+    ...(ownerState.size === "md" && {
+      fontSize: theme.vars.fontSize.md, //sm
+    }),
+    ...(ownerState.size === "lg" && {
+      fontSize: theme.vars.fontSize.lg, //md
+    }),
+  })
+);
+
+const SwitchThumb = styled("span", {
+  name: "JoySwitch",
+  slot: "Thumb",
+  overridesResolver: (props, styles) => styles.thumb,
+})<{ ownerState: SwitchProps }>(({ theme }) => ({
+  "--Icon-fontSize": "calc(var(--Switch-thumb-size) * 0.75)",
+  transition: "left 0.2s",
+  display: "inline-flex",
+  justifyContent: "center",
+  alignItems: "center",
+  position: "absolute",
+  top: "50%",
+  left: "calc(50% - var(--Switch-track-width) / 2 + var(--Switch-thumb-width) / 2 + var(--Switch-thumb-offset))",
+  transform: "translate(-50%, -50%)",
+  width: "var(--Switch-thumb-width)",
+  height: "var(--Switch-thumb-size)",
+  borderRadius: "var(--Switch-thumb-radius)",
+  // boxShadow: "var(--Switch-thumb-shadow)",
+  color: "var(--Switch-thumb-color)",
+  backgroundColor: "var(--Switch-thumb-background)",
+  "&:hover": {
+    border: `5px solid ${theme.palette.primary[100]}`,
+  },
+  [`&.${switchClasses.checked}`]: {
+    left: "calc(50% + var(--Switch-track-width) / 2 - var(--Switch-thumb-width) / 2 - var(--Switch-thumb-offset))",
+  },
 }));
 
-const SwitchThumb = styled('span', {
-  name: 'JoySwitch',
-  slot: 'Thumb',
-  overridesResolver: (props, styles) => styles.thumb,
-})<{ ownerState: SwitchProps }>({
-  '--Icon-fontSize': 'calc(var(--Switch-thumb-size) * 0.75)',
-  transition: 'left 0.2s',
-  display: 'inline-flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  position: 'absolute',
-  top: '50%',
-  left: 'calc(50% - var(--Switch-track-width) / 2 + var(--Switch-thumb-width) / 2 + var(--Switch-thumb-offset))',
-  transform: 'translate(-50%, -50%)',
-  width: 'var(--Switch-thumb-width)',
-  height: 'var(--Switch-thumb-size)',
-  borderRadius: 'var(--Switch-thumb-radius)',
-  boxShadow: 'var(--Switch-thumb-shadow)',
-  color: 'var(--Switch-thumb-color)',
-  backgroundColor: 'var(--Switch-thumb-background)',
-  [`&.${switchClasses.checked}`]: {
-    left: 'calc(50% + var(--Switch-track-width) / 2 - var(--Switch-thumb-width) / 2 - var(--Switch-thumb-offset))',
-  },
-});
-
-const SwitchStartDecorator = styled('span', {
-  name: 'JoySwitch',
-  slot: 'StartDecorator',
+const SwitchStartDecorator = styled("span", {
+  name: "JoySwitch",
+  slot: "StartDecorator",
   overridesResolver: (props, styles) => styles.startDecorator,
 })<{ ownerState: SwitchProps }>({
-  display: 'inline-flex',
-  padding: '0px var(--Switch-decorator-paddingX)',
+  display: "inline-flex",
+  padding: "0px var(--Switch-decorator-paddingX)",
 });
 
-const SwitchEndDecorator = styled('span', {
-  name: 'JoySwitch',
-  slot: 'EndDecorator',
+const SwitchEndDecorator = styled("span", {
+  name: "JoySwitch",
+  slot: "EndDecorator",
   overridesResolver: (props, styles) => styles.endDecorator,
 })<{ ownerState: SwitchProps }>({
-  display: 'inline-flex',
-  padding: '0px var(--Switch-decorator-paddingX)',
+  display: "inline-flex",
+  padding: "0px var(--Switch-decorator-paddingX)",
 });
 
-const Switch = React.forwardRef<HTMLSpanElement, SwitchProps>(function Switch(inProps, ref) {
+const Switch = React.forwardRef<HTMLSpanElement, SwitchProps>(function Switch(
+  inProps,
+  ref
+) {
   const props = inProps;
   const {
     checked: checkedProp,
@@ -228,8 +247,8 @@ const Switch = React.forwardRef<HTMLSpanElement, SwitchProps>(function Switch(in
     readOnly: readOnlyProp,
     required,
     color,
-    variant = 'solid',
-    size = 'md',
+    variant = "solid",
+    size = "md",
     startDecorator,
     endDecorator,
     ...otherProps
@@ -246,7 +265,8 @@ const Switch = React.forwardRef<HTMLSpanElement, SwitchProps>(function Switch(in
     readOnly: readOnlyProp,
   };
 
-  const { getInputProps, checked, disabled, focusVisible, readOnly } = useSwitch(useSwitchProps);
+  const { getInputProps, checked, disabled, focusVisible, readOnly } =
+    useSwitch(useSwitchProps);
 
   const ownerState = {
     ...props,
@@ -254,7 +274,7 @@ const Switch = React.forwardRef<HTMLSpanElement, SwitchProps>(function Switch(in
     disabled,
     focusVisible,
     readOnly,
-    color: checked ? color || 'primary' : color || 'neutral',
+    color: checked ? color || "primary" : color || "neutral",
     variant,
     size,
   };
@@ -274,9 +294,14 @@ const Switch = React.forwardRef<HTMLSpanElement, SwitchProps>(function Switch(in
           aria-hidden="true"
           {...componentsProps.startDecorator}
           ownerState={ownerState}
-          className={clsx(classes.input, componentsProps.startDecorator?.className)}
+          className={clsx(
+            classes.input,
+            componentsProps.startDecorator?.className
+          )}
         >
-          {typeof startDecorator === 'function' ? startDecorator(ownerState) : startDecorator}
+          {typeof startDecorator === "function"
+            ? startDecorator(ownerState)
+            : startDecorator}
         </SwitchStartDecorator>
       )}
 
@@ -309,9 +334,14 @@ const Switch = React.forwardRef<HTMLSpanElement, SwitchProps>(function Switch(in
           aria-hidden="false"
           {...componentsProps.endDecorator}
           ownerState={ownerState}
-          className={clsx(classes.input, componentsProps.endDecorator?.className)}
+          className={clsx(
+            classes.input,
+            componentsProps.endDecorator?.className
+          )}
         >
-          {typeof endDecorator === 'function' ? endDecorator(ownerState) : endDecorator}
+          {typeof endDecorator === "function"
+            ? endDecorator(ownerState)
+            : endDecorator}
         </SwitchEndDecorator>
       )}
     </SwitchRoot>
@@ -340,7 +370,7 @@ Switch.propTypes /* remove-proptypes */ = {
    * @default 'neutral'
    */
   color: PropTypes /* @typescript-to-proptypes-ignore */.oneOfType([
-    PropTypes.oneOf(['danger', 'info', 'primary', 'success', 'warning']),
+    PropTypes.oneOf(["danger", "info", "primary", "success", "warning"]),
     PropTypes.string,
   ]),
   /**
@@ -412,7 +442,7 @@ Switch.propTypes /* remove-proptypes */ = {
    * @default 'md'
    */
   size: PropTypes /* @typescript-to-proptypes-ignore */.oneOfType([
-    PropTypes.oneOf(['sm', 'md', 'lg']),
+    PropTypes.oneOf(["sm", "md", "lg"]),
     PropTypes.string,
   ]),
   /**
@@ -426,7 +456,9 @@ Switch.propTypes /* remove-proptypes */ = {
    * The system prop that allows defining system overrides as well as additional CSS styles.
    */
   sx: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])),
+    PropTypes.arrayOf(
+      PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])
+    ),
     PropTypes.func,
     PropTypes.object,
   ]),
@@ -435,7 +467,7 @@ Switch.propTypes /* remove-proptypes */ = {
    * @default 'solid'
    */
   variant: PropTypes /* @typescript-to-proptypes-ignore */.oneOfType([
-    PropTypes.oneOf(['outlined', 'plain', 'soft', 'solid']),
+    PropTypes.oneOf(["outlined", "plain", "soft", "solid"]),
     PropTypes.string,
   ]),
 } as any;
