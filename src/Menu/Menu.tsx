6e4a9878931d46c56c06689b00bb636ae16c6fae
Kunal Pant <kunalpant@intugine.com>
2022-07-28 12:57:09 +0530

    Added initial code for tab, tabList, tabPanel and Tabs

--- src/Menu/Menu.tsx
+++ src/Menu/Menu.tsx
@@ -161,7 +161,14 @@ const Menu = React.forwardRef(function Menu(inProps, ref) {
     <PopperUnstyled {...rootProps}>
       <MenuUnstyledContext.Provider value={contextValue}>
         <RowListContext.Provider value={false}>
-          {children}
+          {React.Children.map(children, (child, index) =>
+            React.isValidElement(child)
+              ? React.cloneElement(child, {
+                  // to let MenuItem knows when to apply margin(Inline|Block)Start
+                  ...(index === 0 && { "data-first-child": "" }),
+                })
+              : child
+          )}
         </RowListContext.Provider>
       </MenuUnstyledContext.Provider>
     </PopperUnstyled>
